1d89cdf63a88b5799607fd172b489bd5dcdd97ee
Leonid <Chunl3e666@yandex.ru>
2020-12-07 16:10:55 +0300

    Правки 07.12

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,20 @@
-// import $ from 'jquery' // import module example (npm i -D jquery)
+//import $ from 'jquery' // import module example (npm i -D jquery)
+import $ from '../libs/mmenu-light-master/dist/mmenu-light'
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
-
+const menu = new MmenuLight(
+    document.querySelector("#my-menu")
+);
+const navigation = menu.navigation({
+    title: "Меню",
+    theme: "dark"
+});
+const drawer = menu.offcanvas();
+document.querySelector('a[href="#my-menu"]')
+    .addEventListener('click', (evnt) =>{
+        evnt.preventDefault();
+        drawer.open();
+    });
 })
